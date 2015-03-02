a402ebd6734dc2237b15b6ce2c6aa61c0d3ad661
sarahadema <SarahAdema@Sarahs-MacBook-Pro.local>
2015-03-02 18:01:34 -0500

    Linked files and started animtion
    Added initial code, started animation for the first object

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+var $btnCollapseExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
+
+$btnShowHide.on('.click', function(){
+	$box.toggleClass('js-box-show-hide');
+})
+
+$btnMove.on('.click',function(){
+	
+})
