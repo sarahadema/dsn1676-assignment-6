e93e7c2c7022d6713d5e9a76f114a7d0c3bcdb76
sarahadema <SarahAdema@Sarahs-MacBook-Pro.local>
2015-03-02 18:22:55 -0500

    Transitions
    Added transitions and style the different buttons and divs to get the
    objects to perform properly

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
+
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
+
 var $btnCollapseExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
+
 var $btnBounce = $('.btn-bounce');
 var $circle = $('.circle');
+
 var $btnAppend = $('.btn-append');
 var $list = $('.list');
 
-$btnShowHide.on('.click', function(){
+$btnShowHide.on('click', function(){
 	$box.toggleClass('js-box-show-hide');
 })
 
-$btnMove.on('.click',function(){
-	
+$btnMove.on('click',function(){
+	$diamond.toggleClass('js-diamond');
+})
+
+$btnCollapseExpand.on('click',function(){
+	$panel.toggleClass('js-panel');
 })
